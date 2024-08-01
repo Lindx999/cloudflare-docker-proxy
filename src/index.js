8c06f33b6f9828b1660833c780dee5de80499d51
Lindx999 <xjun987@gmail.com>
2024-08-01 10:25:52 +0800

    Update index.js
    change domain

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.iiep.cn": dockerHub,
+  "quay.iiep.cn": "https://quay.io",
+  "gcr.iiep.cn": "https://gcr.io",
+  "k8s-gcr.iiep.cn": "https://k8s.gcr.io",
+  "k8s.iiep.cn": "https://registry.k8s.io",
+  "ghcr.iiep.cn": "https://ghcr.io",
+  "cloudsmith.iiep.cn": "https://docker.cloudsmith.io",
+  "ecr.iiep.cn": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
